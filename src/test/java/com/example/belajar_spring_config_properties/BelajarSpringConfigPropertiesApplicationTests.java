1f92a92ce7c2d4694a1e696b830d4db90143f9ff
puspakirana <puspakiranastv@gmail.com>
2025-03-05 20:05:30 +0700

    init project

---
+++ src/test/java/com/example/belajar_spring_config_properties/BelajarSpringConfigPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.belajar_spring_config_properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringConfigPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
