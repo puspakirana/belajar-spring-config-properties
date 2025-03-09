892d04df370313c360a25ef72dcb094d796df8dc
puspakirana <puspakiranastv@gmail.com>
2025-03-09 20:18:09 +0700

    Spring Boot Message Source

---
+++ src/test/java/com/example/belajar_spring_config_properties/springbootmessagesource/SpringBootMessageSourceTest.java
@@ -0,0 +1,45 @@
+package com.example.belajar_spring_config_properties.springbootmessagesource;
+
+import lombok.Setter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.MessageSourceAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@SpringBootTest(classes = SpringBootMessageSourceTest.TestApplication.class)
+public class SpringBootMessageSourceTest {
+
+    @Autowired
+    private TestApplication.SampleSource sampleSource;
+
+    @Test
+    void testHelloEko() {
+        Assertions.assertEquals("Hello Eko", sampleSource.helloEko(Locale.ENGLISH));
+        Assertions.assertEquals("Halo Eko", sampleSource.helloEko(new Locale("in_ID")));
+    }
+
+    @SpringBootApplication
+    public static class TestApplication {
+
+        @Component
+        public static class SampleSource implements MessageSourceAware {
+
+            private MessageSource messageSource;
+
+            public String helloEko(Locale locale) {
+                return messageSource.getMessage("hello", new Object[]{"Eko"}, locale);
+            }
+
+            @Override
+            public void setMessageSource(MessageSource messageSource) {
+                this.messageSource = messageSource;
+            }
+        }
+    }
+}
